e591957e4bef89a02b7200b55dc7812be9302c3d
ctrador <chad.trador@interapthq.com>
2016-10-14 13:11:30 -0400

    put birds and animals in

---
+++ zoolabweek2/zoolabweek2/birds.swift
@@ -0,0 +1,9 @@
+//
+//  birds.swift
+//  zoolabweek2
+//
+//  Created by Chad Trador on 10/14/16.
+//  Copyright © 2016 Chad Trador. All rights reserved.
+//
+
+import Foundation
---
+++ zoolabweek2/zoolabweek2/fish.swift
@@ -0,0 +1,9 @@
+//
+//  fish.swift
+//  zoolabweek2
+//
+//  Created by Chad Trador on 10/14/16.
+//  Copyright © 2016 Chad Trador. All rights reserved.
+//
+
+import Foundation
